48ab879f54279c5bdeb2f5935f91b96b154af8c6
goze <goze.qiu@gmail.com>
2022-07-03 19:47:47 +0800

    feat: clipboard & notification

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	_ "embed"
 	"log"
 
+	"github.com/atotto/clipboard"
+	"github.com/gen2brain/beeep"
 	"github.com/getlantern/systray"
 	"github.com/skratchdot/open-golang/open"
 )
@@ -11,19 +13,36 @@ import (
 //go:embed icon/icon.png
 var icon []byte
 
-func main() {
+var links = map[string]string{
+	"appName": "XTool",
+	"github":  "https://github.com/gozeon",
+	"twitter": "https://twitter.com/gozeonl",
+}
+
+func init() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
+	beeep.Beep(beeep.DefaultFreq, beeep.DefaultDuration)
+
+}
+
+func main() {
 	systray.Run(onReady, onExit)
+}
+
+func sysAlert(msg string) {
+	beeep.Alert(links["appName"], msg, string(icon))
 }
 
 func onReady() {
 	log.Println("onReady")
 	systray.SetTemplateIcon(icon, icon)
-	// systray.SetTitle("xtool")
-	systray.SetTooltip("xtool")
+	systray.SetTooltip(links["appName"])
 
 	github := systray.AddMenuItem("Github", "gozeon")
 	twitter := systray.AddMenuItem("Twitter", "gozeonl")
+
+	alert := systray.AddMenuItem("Alert", "系统通知")
+	clipboardMenu := systray.AddMenuItem("Clipboard", "粘贴板")
 
 	systray.AddSeparator()
 	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
@@ -31,13 +50,18 @@ func onReady() {
 		for {
 			select {
 			case <-github.ClickedCh:
-				log.Println("open", "https://github.com/gozeon")
-				open.Run("https://github.com/gozeon")
+				open.Run(links["github"])
+				log.Println("open", links["github"])
 			case <-twitter.ClickedCh:
-				log.Println("open", "https://twitter.com/gozeonl")
-				open.Run("https://twitter.com/gozeonl")
+				open.Run(links["twitter"])
+				log.Println("open", links["twitter"])
 			case <-mQuit.ClickedCh:
 				systray.Quit()
+			case <-clipboardMenu.ClickedCh:
+				cl, _ := clipboard.ReadAll()
+				log.Println("clipboard", cl)
+			case <-alert.ClickedCh:
+				sysAlert("hello world")
 			}
 		}
 	}()
